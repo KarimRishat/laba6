6201080fd56ee47542eeaca233f1f52566c02063
Rishat <karimr2000@mail.ru>
2021-10-22 01:08:51 +0300

    Добавьте файлы проекта.

---
+++ Metodichka/BankAccount.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Metodichka
+{
+    public enum AccountType { Current, Saving, IncorrectInput };
+    class BankAccount
+    {
+        
+        private uint number;
+        private decimal balance;
+        private AccountType type;
+        private static uint genNum = 1;
+        public void GetInfo()
+        {
+            Console.WriteLine($"Номер счета: {number},\nТип счета: {type},\nБаланс счета: {balance}");
+        }
+        public void CreateAcc()
+        {
+            number = genNum++;
+        }
+        public void SetAccInfo()
+        {
+            bool flag;
+            uint x;
+            decimal y;
+            Console.WriteLine("Введите баланс:");
+            while (flag = !decimal.TryParse(Console.ReadLine(), out y))
+            {
+                Console.WriteLine("Некорректный ввод");
+            }
+            balance = y;
+            Console.WriteLine("Выберите тип счета:\n1.Current;\n2.Saving");
+            while (flag = !uint.TryParse(Console.ReadLine(),out x))
+            {
+                Console.WriteLine("Некорректный ввод");
+            }
+            switch (x)
+            {
+                case 1:
+                    type = AccountType.Current;
+                    break;
+                case 2:
+                    type = AccountType.Saving;
+                        break;
+                default:
+                    type = AccountType.IncorrectInput;
+                    break;
+            }
+        }
+        public void Withdraw(decimal take)
+        {
+            if (balance-take <= 0)
+            {
+                Console.WriteLine("Снять со счета эту сумму нельзя");
+            }
+            else
+            {
+                balance -= take;
+            }
+        }
+        public void AddMoney(decimal add)
+        {
+            balance += add;
+        }
+    }
+}
---
+++ Metodichka/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Metodichka
+{
+    class Program
+    {
+        public enum AccountType { Current, Saving, IncorrectInput };
+        static void Main(string[] args)
+        {
+            BankAccount acc1 = new BankAccount();
+            acc1.CreateAcc();
+            acc1.SetAccInfo();
+            Console.WriteLine("Введите сумму для внесения на баланс счета");
+            decimal x;
+            while (!decimal.TryParse(Console.ReadLine(),out x))
+            {
+                Console.WriteLine("Некорректный ввод");
+            }
+            acc1.AddMoney(x);
+            acc1.GetInfo();
+            BankAccount acc2 = new BankAccount();
+            acc2.CreateAcc();
+            acc2.SetAccInfo();
+            Console.WriteLine("Введите сумму для внесения на баланс счета");
+            while (!decimal.TryParse(Console.ReadLine(), out x))
+            {
+                Console.WriteLine("Некорректный ввод");
+            }
+            acc2.AddMoney(x);
+            acc2.GetInfo();
+            Console.ReadKey();
+        }
+    }
+}
